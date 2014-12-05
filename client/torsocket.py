0de7d4543bfd2b0aa3f9ea787316d1fd9af45aaf
Adam <asv-github@mit.edu>
2014-12-05 18:38:08 -0500

    Added dummy torsocket.py that just uses urllib2.

---
+++ client/torsocket.py
@@ -0,0 +1,13 @@
+"""
+torsocket.py: Provides methods for making connections through tor.
+"""
+
+import urllib2
+
+def starttor():
+	print "torsocket.starttor: NOT IMPLEMENTED YET"
+	pass
+
+def openurl(url):
+	print "torsocket.openurl: NOT IMPLEMENTED YET"
+	return urllib2.urlopen(url)
