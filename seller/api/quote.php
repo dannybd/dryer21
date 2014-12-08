999e8761fc3d461a451185d5b0442f423d55d04d
Daniel M Ben-David <dannybd@mit.edu>
2014-12-07 22:58:14 -0500

    Adding PHP files used to mock out server.

---
+++ seller/api/quote.php
@@ -0,0 +1,9 @@
+<?php
+
+header("Content-type: text/javascript");
+echo json_encode(array(
+  'price' => '0.1BTC', 
+  'addr' => '1BTCBTCBTCBTCBTCBTCBTCBTC',
+));
+
+?>
---
+++ seller/api/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+print "Content-Type: text/plain;charset=utf-8"
+print
+
+print "Hello World!"
