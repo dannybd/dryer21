469b569d80df82415e26a83c0a8d7011f90c0115
Daniel M Ben-David <dannybd@mit.edu>
2014-12-07 22:50:26 -0500

    Added method calls to gen_quote and issue_protobond. They're not ready yet, and this isn't the proper way to do this (still need rpc) but it's a signal of what to do.

--- seller/seller.py
+++ seller/seller.py
@@ -15,16 +15,31 @@ def fetch_init():
 @app.route('/quote', methods=['POST'])
 def fetch_quote():
 	token = request.args.get('token', None)
-	return jsonify(token=token,price='0.1BTC',addr='1TORTORTORTOR')
+	mock = False
+	try:
+		from gen_quote import gen_quote
+		(addr, price) = gen_quote(token=token)
+	except Exception, e:
+		mock = True
+		(addr, price) = ('1DRYER21DRYER21DRYER21', '0.1BTC')
+	finally:
+		return jsonify(token=token,addr=addr,price=price,mock=mock)
 
 @app.route('/protobond', methods=['POST'])
 def fetch_protobond():
 	token = request.args.get('token', None)
-	from random import randint
-	if randint(1, 4) != 1:
-		return jsonify(error='No bitcoin yet...')
-	protobond = 'protobond goes here'
-	return jsonify(protobond=protobond)
+	mock = False
+	try:
+		from issue_protobond import issue_protobond
+		protobond = issue_protobond(token)
+	except Exception, e:
+		mock = True
+		from random import randint
+		if randint(1, 4) != 1:
+			return jsonify(error='No bitcoin yet...',mock=mock)
+		protobond = 'protobond goes here'
+	finally:
+		return jsonify(protobond=protobond,mock=mock)
 
 if __name__ == '__main__':
 	app.run(port=9001)
