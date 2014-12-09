9913feafc58108c1dfdab78f3b4625a54f47946e
Daniel M Ben-David <dannybd@mit.edu>
2014-12-09 06:41:35 -0500

    Fleshed out redeemer templates and flask app structure.

--- redeemer/redeemer.py
+++ redeemer/redeemer.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import make_response, render_template, request, json, url_for
+from flask import make_response, redirect, render_template, request, json, url_for
 jsonify = json.jsonify
 
 app = Flask(__name__)
@@ -13,14 +13,17 @@ app.add_url_rule(
 def index():
 	return render_template('redeemer.html')
 
-# FIXME: Don't use GET
 @app.route('/bond', methods=['GET', 'POST'])
-def fetch_protobond():
+def redeem_bond():
+	if request.method == 'GET':
+		return redirect(url_for(''))
 	bond = request.args.get('bond', None)
+	if not bond:
+		return render_template('bond_error.html', err_msg='No bond supplied!')
 	to_addr = request.args.get('to_addr', None)
-	if not bond or not to_addr:
-		return jsonify(error=True, success=False)
-	return jsonify(success=True, msg='Thanks!')
+	if not to_addr:
+		return render_template('bond_error.html', err_msg='No destination bitcoin address supplied!')
+	return render_template('bond_success.html', to_addr=to_addr)
 
 if __name__ == '__main__':
 	app.run(port=9002)
